c0c971991905001a6dc7279d5535b3472779fd8e
Armaan Amatya <armaanamatya2014@gmail.com>
2025-10-14 22:09:53 -0500

    changeet

--- src/App.tsx
+++ src/App.tsx
@@ -174,7 +174,25 @@ function App() {
             <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-.904.732-1.636 1.636-1.636h.749L12 10.855l9.615-7.034h.749c.904 0 1.636.732 1.636 1.636z"/>
           </svg>
         </a>
+<<<<<<< Updated upstream
         {/* <div className="toolbar-separator"></div>
+=======
+        <div className="toolbar-separator"></div>
+<<<<<<< Updated upstream
+=======
+        <a 
+          href="/Armaan-Amatya-Resume.pdf" 
+          download="Armaan-Amatya-Resume.pdf"
+          className="toolbar-btn"
+          title="Download Resume"
+        >
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
+          </svg>
+        </a>
+        {/* <div className="toolbar-separator"></div>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         <button 
           className="toolbar-btn theme-btn" 
           title="Toggle theme"
