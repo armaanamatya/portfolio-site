3c2c86bdc4a96f44b86a30a14a40b3468f30c3ab
Armaan Amatya <armaanamatya2014@gmail.com>
2025-11-22 18:28:01 -0600

    updates

---
+++ src/components/HighlightWidget.tsx
@@ -0,0 +1,18 @@
+import './HighlightWidget.css';
+
+interface HighlightWidgetProps {
+  title: string;
+  description: React.ReactNode;
+}
+
+function HighlightWidget({ title, description }: HighlightWidgetProps) {
+  return (
+    <div className="highlight-widget">
+      <h3 className="highlight-title">{title}</h3>
+      <div className="highlight-description">{description}</div>
+    </div>
+  );
+}
+
+export default HighlightWidget;
+
